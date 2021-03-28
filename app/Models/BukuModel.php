fb90df475215367c54d29ffaa1bb26b901688e0b
swlgnt <galandaru.swalaganata@gmail.com>
2021-03-28 16:05:24 +0700

    add getBuku

--- app/Models/BukuModel.php
+++ app/Models/BukuModel.php
@@ -9,5 +9,13 @@ class BukuModel extends Model
     protected $table = 'buku';
     //protected $primaryKey = 'id'; // tidak perlu karena di basemodel sudah ada
     protected $useTimestamps = true;
+    public function getBuku($slug = false)
+    {
+        if ($slug == false) {
+            return $this->findAll();
+        }
+
+        return $this->where(['slug'=>$slug])->first();
+    }
 }
 ?>
