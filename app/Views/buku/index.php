a66ef167b69d63195480924915c9ab073ddd5698
swlgnt <galandaru.swalaganata@gmail.com>
2021-03-28 21:29:26 +0700

    add flashData, nomor

--- app/Views/buku/index.php
+++ app/Views/buku/index.php
@@ -2,6 +2,17 @@
 <?= $this->section('content'); ?>
 <div class="container">
     <h1>Buku</h1>
+    <a href="/buku/tambah" class="btn btn-primary mb-3">Tambah Data</a>
+    <?php if(session()->getFlashData('pesan')) : ?>
+    <div class="alert alert-success" role="alert">
+        <?= session()->getFlashData('pesan'); ?>
+    </div>
+    <?php endif; ?>
+    <?php if(session()->getFlashData('pesan2')) : ?>
+    <div class="alert alert-warning" role="alert">
+        <?= session()->getFlashData('pesan2'); ?>
+    </div>
+    <?php endif; ?>
     <div class="row">
         <div class="col">
             <table class="table">
@@ -14,14 +25,16 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php $i=1 ?>
                     <?php foreach ($buku as $bk) : ?>
                     <tr>
-                        <th scope="row">1</th>
+                        <th scope="row"><?= $i; ?></th>
                         <td><img src="/img/<?= $bk['sampul']; ?>" alt="" class="sampul"></td>
                         <td><?= $bk['judul']; ?></td>
-                        <td><a href="/buku/<?= $bk['slug']; ?>" class="btn btn-success">Lihat detail buku</a></td>
+                        <td><a href="/buku/<?= $bk['slug']; ?>" class="btn btn-success">Lihat detail buku</a>
+                        </td>
                     </tr>
-                    <?php endforeach; ?>
+                    <?php $i++; endforeach; ?>
                 </tbody>
             </table>
         </div>
