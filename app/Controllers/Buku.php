4746812d3bed6ed23e85637a175be6a358d7ff9b
swlgnt <galandaru.swalaganata@gmail.com>
2021-03-28 16:05:10 +0700

    add detail buku

--- app/Controllers/Buku.php
+++ app/Controllers/Buku.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use Prophecy\Doubler\Generator\Node\ReturnTypeNode;
 use App\Models;
 use App\Models\BukuModel;
+use PHPUnit\TextUI\XmlConfiguration\MoveWhitelistDirectoriesToCoverage;
 
 class Buku extends BaseController
 {
@@ -27,4 +28,19 @@ class Buku extends BaseController
         // dd($bukuModel->findAll()); //untuk menampilkan dummy data
         return view('buku/index',$data);
     }    
+
+    public function detail($slug)
+    {
+        // $buku = $this->bukuModel->findAll();
+        $buku = $this->bukuModel->getBuku($slug);
+        $data = [
+            'title' => 'Koleksi Buku',
+            'buku' => $buku
+            // 'buku' => $this->bukuModel->getBuku($slug)
+        ];
+        // dd($buku);
+        // $bukuModel->findAll();
+        // dd($bukuModel->findAll()); //untuk menampilkan dummy data
+        return view('buku/detail',$data);
+    }
 }
