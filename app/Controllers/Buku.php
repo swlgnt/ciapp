f29900fce4521b51e3eb6fba5641742117910326
swlgnt <galandaru.swalaganata@gmail.com>
2021-03-28 14:27:28 +0700

    tambah bukumodel

--- app/Controllers/Buku.php
+++ app/Controllers/Buku.php
@@ -3,14 +3,28 @@
 namespace App\Controllers;
 
 use Prophecy\Doubler\Generator\Node\ReturnTypeNode;
+use App\Models;
+use App\Models\BukuModel;
 
 class Buku extends BaseController
 {
-public function index()
-{   
-    $data = [
-        'title' => 'Koleksi Buku'
-    ];
-    return view('buku/index',$data);
-}    
+    protected $bukuModel;
+    public function __construct()
+    {
+        // $bukuModel = new \App\Models\BukuModel();
+        $this->bukuModel = new BukuModel();
+    }
+    public function index()
+    {   
+        $buku = $this->bukuModel->findAll();
+        
+        $data = [
+            'title' => 'Koleksi Buku',
+            'buku' => $buku
+        ];
+        // dd($buku);
+        // $bukuModel->findAll();
+        // dd($bukuModel->findAll()); //untuk menampilkan dummy data
+        return view('buku/index',$data);
+    }    
 }
