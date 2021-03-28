7aacd45e286aa26f072bf97fecafb89f9e479053
swlgnt <galandaru.swalaganata@gmail.com>
2021-03-28 21:26:49 +0700

    add slug error, method tambah, simpan, hapus, ubah, update

--- app/Controllers/Buku.php
+++ app/Controllers/Buku.php
@@ -38,9 +38,107 @@ class Buku extends BaseController
             'buku' => $buku
             // 'buku' => $this->bukuModel->getBuku($slug)
         ];
+        if (empty($data['buku'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul buku tidak ditemukan');
+        }
+        
         // dd($buku);
         // $bukuModel->findAll();
         // dd($bukuModel->findAll()); //untuk menampilkan dummy data
         return view('buku/detail',$data);
+    }
+
+    public function tambah()
+    {
+        // session();
+        $data = [
+            'title' => 'Tambah Data Buku',
+            'valid' => \Config\Services::validation()
+        ];
+        return view('buku/create',$data);
+    }
+
+    public function simpan()
+    {
+        //validasi
+        if (!$this->validate([
+            'judul' => [
+                'rules' => 'required|is_unique[buku.judul]',
+                'errors' => [
+                    'required' => '{field} buku harus diisi.',
+                    'is_unique' => '{field} sudah ada di database.'
+                ]
+            ]
+        ])) {
+            $valid = \Config\Services::validation();
+            // dd($valid);
+            return redirect()->to('/buku/tambah')->withInput()->with('validation', $valid);
+        }
+
+        $slug=url_title($this->request->getVar('judul'),'-',true);
+        $this->bukuModel->save([
+            'judul'=>$this->request->getVar('judul'),
+            'slug'=>$slug,
+            'penulis'=>$this->request->getVar('penulis'),
+            'penerbit'=>$this->request->getVar('penerbit'),
+            'sampul'=>$this->request->getVar('sampul')
+        ]);
+
+        session()->setFlashData('pesan','Data berhasil disimpan!');
+        
+        return redirect()->to('/buku');
+    }
+
+    public function hapus($id){
+        $this->bukuModel->delete($id);
+        session()->setFlashData('pesan2','Data berhasil dihapus!');
+        return redirect()->to('/buku');
+    }
+
+    public function ubah($slug){
+         $data = [
+            'title' => 'Form Ubah Data Buku',
+            'valid' => \Config\Services::validation(),
+            'buku' => $this->bukuModel->getBuku($slug)
+        ];
+        return view('buku/ubah',$data);
+    }
+
+    public function update($id)
+    {
+        $judulLama = $this->bukuModel->getBuku($this->request->getVar('slug'));
+        if ($judulLama['judul'] == $this->request->getVar('judul')) {
+            $rule_judul = 'required';
+        } else {
+            $rule_judul = 'required|is_unique[buku.judul]';
+        }
+         //validasi
+        if (!$this->validate([
+            'judul' => [
+                'rules' => $rule_judul,
+                'errors' => [
+                    'required' => '{field} buku harus diisi.',
+                    'is_unique' => '{field} sudah ada di database.'
+                ]
+            ]
+        ])) {
+            $valid = \Config\Services::validation();
+            // dd($valid);
+            return redirect()->to('/buku/ubah/' . $this->request->getVar('slug'))->withInput()->with('validation', $valid);
+        }
+        
+        $slug=url_title($this->request->getVar('judul'),'-',true);
+        $this->bukuModel->save([
+            'id'=>$id,
+            'judul'=>$this->request->getVar('judul'),
+            'slug'=>$slug,
+            'penulis'=>$this->request->getVar('penulis'),
+            'penerbit'=>$this->request->getVar('penerbit'),
+            'sampul'=>$this->request->getVar('sampul')
+        ]);
+
+        session()->setFlashData('pesan','Data berhasil diubah!');
+        
+        return redirect()->to('/buku');
     }
 }
