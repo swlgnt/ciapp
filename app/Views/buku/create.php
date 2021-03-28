0a6d8e705e895eb33bcfc4d31133c81430bcc9bb
swlgnt <galandaru.swalaganata@gmail.com>
2021-03-28 21:27:41 +0700

    initial create

---
+++ app/Views/buku/create.php
@@ -0,0 +1,41 @@
+<?= $this->extend('layout/template'); ?>
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h2>Tambah Data</h2>
+            <!-- <?//= $valid->listErrors(); ?> -->
+            <form action="/buku/simpan" method="POST">
+                <?= csrf_field(); ?>
+                <div class="mb-3">
+                    <label for="formGroupExampleInput" class="form-label">Judul</label>
+                    <input type="text" name="judul"
+                        class="form-control <?= ($valid->hasError('judul')) ? 'is-invalid' : ''; ?>"
+                        id="formGroupExampleInput" placeholder="" autofocus value="<?= old('judul'); ?>">
+                    <div class="invalid-feedback">
+                        <?= $valid->getError('judul'); ?>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="formGroupExampleInput2" class="form-label">Penulis</label>
+                    <input type="text" name="penulis" class="form-control" id="formGroupExampleInput2" placeholder=""
+                        value="<?= old('penulis'); ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="formGroupExampleInput2" class="form-label">Penerbit</label>
+                    <input type="text" name="penerbit" class="form-control" id="formGroupExampleInput2" placeholder=""
+                        value="<?= old('penerbit'); ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="formFile" class="form-label">Sampul</label>
+                    <input class="form-control" name="sampul" type="file" id="formFile" value="<?= old('sampul'); ?>">
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
